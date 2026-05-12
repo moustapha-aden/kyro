fc7e4e9174586f7ed74591983ed8694144c8275a
Filsan <filsanfouad2002@gmail.com>
2026-05-12 16:13:26 +0300

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,15 +17,15 @@ export default function Hero() {
           <source src={bgback} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+        
         {/* Overlay pour le contraste et le style "Premium" */}
-        <div className="absolute inset-0 bg-black/50 backdrop-blur-[2px]"></div>
+        <div className="absolute inset-0 bg-black/5 "></div>
       </div>
 
       {/* 2. Contenu Textuel (Design Awwwards Style) */}
       <div className="relative z-10 container mx-auto px-6 text-center">
         <h1 className="text-white text-6xl md:text-7xl lg:text-7xl font-extralight tracking-[-0.05em] leading-none">
          Votre infrastructure IT, notre expertise
-         
         </h1>
         
         <p className="mt-8 text-gray-300 text-lg md:text-xl max-w-2xl mx-auto font-light tracking-wide">
